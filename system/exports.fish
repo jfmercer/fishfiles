eb42082c6bacd5da6173696d4856ced023146aa4
John F. Mercer <john.f.mercer@gmail.com>
2015-12-30 10:45:54 -0500

    Changes JAVA_HOME to Java 8 from Java 7
    * Also cleans up formatting

--- system/exports.fish
+++ system/exports.fish
@@ -1,22 +1,27 @@
 # Make vim the default editor
 set -x EDITOR "vim"
+
 # Don’t clear the screen after quitting a manual page
 # set -x MANPAGER "less -X"
+
 # Highlight section titles in manual pages
 # set -x LESS_TERMCAP_md "$ORANGE"
+
 # set $SHELL
 set -x SHELL "/usr/local/bin/fish"
+
 # Larger zsh history (allow 32³ entries; default is 500)
 # set -x HISTSIZE 32768
 # set -x HISTFILESIZE $HISTSIZE
 # set -x HISTCONTROL ignoredups
-# # Make some commands not show up in history
+
+# Make some commands not show up in history
 # set -x HISTIGNORE "ls:ls *:cd:cd -:pwd;exit:date:* --help"
 
-set -x JAVA6_HOME (eval /usr/libexec/java_home -v 1.6)
-set -x JAVA7_HOME (eval /usr/libexec/java_home -v 1.7)
-# set -x JAVA8_HOME (eval /usr/libexec/java_home -v 1.8)
-set -x JAVA_HOME $JAVA7_HOME
+# set -x JAVA6_HOME (eval /usr/libexec/java_home -v 1.6)
+# set -x JAVA7_HOME (eval /usr/libexec/java_home -v 1.7)
+set -x JAVA8_HOME (eval /usr/libexec/java_home -v 1.8)
+set -x JAVA_HOME $JAVA8_HOME
 # set -x MAVEN_HOME /usr/local/Cellar/maven/
 
 # Disables automatic cowsay on Ansible
