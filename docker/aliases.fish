abeb9c8b9064e760315fddf61c925b6f8db917b6
John F. Mercer <john.f.mercer@gmail.com>
2016-05-24 09:49:06 -0400

    Adds docker support

---
+++ docker/aliases.fish
@@ -0,0 +1,14 @@
+# Docker Aliases
+
+alias vu 'vagrant up'
+
+alias d 'docker'
+alias dc 'docker-compose'
+alias dcu 'docker-compose up'
+# alias dm 'docker-machine'
+# alias dmpd 'docker-machine stop default'
+# alias dmrd 'docker-machine restart default; eval (docker-machine env default)'
+# alias dmsd 'docker-machine start default; eval (docker-machine env default)'
+# alias dmpv 'docker-machine stop vmware'
+# alias dmrv 'docker-machine restart vmware; eval (docker-machine env vmware)'
+# alias dmsv 'docker-machine start vmware; eval (docker-machine env vmware)'
